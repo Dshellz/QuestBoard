e910661ac3679bd86a2a70b329697e5fbd4407b3
Dshellz <davidsanchez38550@gmail.com>
2025-05-22 14:57:51 +0200

    Update example.spec.ts

--- QuestBoard/__test__/example.spec.ts
+++ QuestBoard/__test__/example.spec.ts
@@ -54,12 +54,3 @@ test.describe("main tests", () => {
     await expect(page.getByText("example value")).not.toBeVisible;
   });
 });
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
